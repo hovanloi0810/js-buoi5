8b2fdbb027cf5bf5404491223bc1b4b9da229c2b
hovanloi0810 <hovanloi0810@gmail.com>
2022-07-28 00:25:01 +0700

    done

--- main.js
+++ main.js
@@ -20,13 +20,13 @@ document.getElementById("btnResult1").onclick = function () {
     let scoreObject = +document.getElementById("chooseObject").value;
     let totalScore = inputScore1 + inputScore2 + inputScore3 + scoreLocal + scoreObject;
     let result = document.getElementById("txtResult");
-    if (totalScore >= benchMark && inputScore1 > 0 && inputScore2 > 0 && inputScore3 > 0 ) {
+    if (totalScore >= benchMark && inputScore1 > 0 && inputScore2 > 0 && inputScore3 > 0) {
         result.innerHTML = "Bạn: đậu " + " Điểm bạn là: " + totalScore;
     }
     else {
         result.innerHTML = "Bạn: rớt" + " Điểm bạn là: " + totalScore;
     }
-    
+
 
 }
 // =========================================
@@ -54,18 +54,18 @@ document.getElementById("btnResult2").onclick = function () {
     } else {
         if (inputKw <= 50) {
             priceKw = 500 * inputKw;
-        } 
-        else if(inputKw <= 100) {
-            priceKw = 500 * inputKw + (inputKw-50) * 650;
         }
-        else if(inputKw <= 150) {
-            priceKw = 500 * 50 + 650 * 50 + ((inputKw-100) * 850);
+        else if (inputKw <= 100) {
+            priceKw = 500 * inputKw + (inputKw - 50) * 650;
         }
-        else if(inputKw <= 200) {
-            priceKw = 500 * 50 + 650 * 50 + 850 * 100 + ((inputKw-200) * 1100)
+        else if (inputKw <= 150) {
+            priceKw = 500 * 50 + 650 * 50 + ((inputKw - 100) * 850);
+        }
+        else if (inputKw <= 200) {
+            priceKw = 500 * 50 + 650 * 50 + 850 * 100 + ((inputKw - 200) * 1100)
         }
         else {
-            priceKw = 500 * 50 + 650 * 50 + 850 * 100 + 1100 * 150 + ((inputKw-200) * 1300)
+            priceKw = 500 * 50 + 650 * 50 + 850 * 100 + 1100 * 150 + ((inputKw - 200) * 1300)
         }
         txtResult2.innerHTML = "Tên: " + inputName + " " + "Tiền điện: " + priceKw.toLocaleString();
     }
@@ -103,26 +103,26 @@ document.getElementById("btnResult3").onclick = function () {
     let incomeTaxes = totalSalary - 4e6 - numberUser * 16e5;
     let personalIncome = 0;
     let txtResult3 = document.getElementById("txtResult3");
- 
+
     if (incomeTaxes <= 0) {
         alert("nhập giá trị sai");
     } else {
         if (incomeTaxes <= 6e7) {
             personalIncome = incomeTaxes * 0.05;
-        } 
-        else if(incomeTaxes <= 12e7) {
-            personalIncome = 6e7 * 0.05 + (incomeTaxes - 6e7) * 0.1
-        } 
-        else if(incomeTaxes <= 21e7) {
-            personalIncome = 6e7 * 0.05 + 6e7 * 0.1 + (incomeTaxes-12e7) * 0.15
         }
-        else if(incomeTaxes <= 384e6) {
+        else if (incomeTaxes <= 12e7) {
+            personalIncome = 6e7 * 0.05 + (incomeTaxes - 6e7) * 0.1
+        }
+        else if (incomeTaxes <= 21e7) {
+            personalIncome = 6e7 * 0.05 + 6e7 * 0.1 + (incomeTaxes - 12e7) * 0.15
+        }
+        else if (incomeTaxes <= 384e6) {
             personalIncome = 6e7 * 0.05 + 6e7 * 0.1 + 9e7 * 0.15 + (incomeTaxes - 21e7) * 0.2
         }
-        else if(incomeTaxes <= 624e6) {
+        else if (incomeTaxes <= 624e6) {
             personalIncome = 6e7 * 0.05 + 6e7 * 0.1 + 9e7 * 0.15 + 174e6 * 0.2 + (incomeTaxes - 384e6) * 0.25
         }
-        else if(incomeTaxes <= 96e7) {
+        else if (incomeTaxes <= 96e7) {
             personalIncome = 6e7 * 0.05 + 6e7 * 0.1 + 9e7 * 0.15 + 174e6 * 0.2 + 24e7 * 0.25 + (incomeTaxes - 624e6) * 0.3
         }
         else {
@@ -130,5 +130,50 @@ document.getElementById("btnResult3").onclick = function () {
         }
         txtResult3.innerHTML = "Họ tên:" + fullName + "Tiền thuế thu nhập cá nhân: " + personalIncome.toLocaleString();
     }
-    
+
+}
+
+// ===================================================
+/*
+Bài 4: Tính tiền cáp
+INPUT:
+    - Mã khách hàng, loại khách hàng, số kết nối, số kênh
+cao cấp.
+HANDLE:
+    - Tạo biến lưu Mã khách hàng, loại khách hàng, số kết nối, số kênh cao cấp, tiền cáp
+    - if (people)       
+        tiền cáp = 4.5 + 20.5 + 7.5 * số kết nối;
+    - else 
+        số kết nối <= 10 : tiền cáp = 15 + 75 + 50*số kênh cao cấp;
+        còn lại: tiền cáp = 15 + 75 + 50*số kênh cao cấp + (số kết nối-10) * 5;
+OUTPUT
+    - In thông tin và số tiền cáp
+*/
+document.getElementById("btnResult4").onclick = function () {
+    let selCustomer = document.getElementById("selCustomer").value;
+    let codeCustomer = document.getElementById("codeCustomer").value;
+    let numberChanel = document.getElementById("numberChanel").value;
+    let numberConnect = document.getElementById("numberConnect").value;
+    let cableMoney = 0;
+    let txtResult4 = document.getElementById("txtResult4");
+
+
+    if (selCustomer == "people") {
+        cableMoney = 4.5 + 20.5 + 7.5 * numberChanel;
+        txtResult4.innerHTML = "Mã khách hàng: " + codeCustomer + " Tiền cáp: $" + cableMoney
+    }
+    else if(selCustomer == "enterprise") {
+        if (numberConnect <= 10) {
+            cableMoney = 15 + 75 + 50 * numberChanel;
+            txtResult4.innerHTML = "Mã khách hàng: " + codeCustomer + " Tiền cáp: $" + cableMoney
+        }
+        else {
+            cableMoney = 15 + 75 + 50 * numberChanel + (numberConnect - 10) * 5;
+            txtResult4.innerHTML = "Mã khách hàng: " + codeCustomer + " Tiền cáp: $" + cableMoney
+        }
+    }
+    else {
+        alert("chọn khách hàng")
+    }
+
 }
